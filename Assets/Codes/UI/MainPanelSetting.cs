6248007e9cfc3ff70437b2636dfd56cbde7458be
Chickenyours <1662036952.@qq.com>
2023-01-11 09:35:25 +0800

    修改Panel

---
+++ Assets/Codes/UI/MainPanelSetting.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using QFramework;
+
+public class MainPanelSetting : GameControll
+{
+    [HideInInspector]
+    public bool isSettingOpen = false;
+    private void Awake()
+    {
+        transform.Find("OpenSettingBin").gameObject.GetComponent<Button>().onClick.AddListener(OpenSetting);
+    }
+    private void OpenSetting()
+    {
+        if (!isSettingOpen)
+        {
+            ResHelp.LoadAsync<GameObject>("Items/UI/SettingPanel", o => 
+            {
+                o.transform.SetParent(GameObject.Find("Canvas").transform);
+                //定义SettingPanel的锚点坐标
+                (o.transform as RectTransform).anchoredPosition = Vector2.zero;
+            });
+            isSettingOpen = true;
+        }
+    }
+}
---
+++ Assets/Codes/UI/SettingPanel.cs
@@ -0,0 +1,19 @@
+﻿using QFramework;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class SettingPanel : GameControll
+{
+    //保存了MianPanelSetting的引用
+    private MainPanelSetting MainPanel;
+    private void Awake()
+    {
+        MainPanel = GameObject.Find("MainPanel").GetComponent<MainPanelSetting>();
+        transform.Find("BackBin").gameObject.GetComponent<Button>().onClick.AddListener(OnCloseBin);
+    }
+    private void OnCloseBin()
+    {
+        MainPanel.isSettingOpen = false;
+        Destroy(gameObject);
+    }
+}
